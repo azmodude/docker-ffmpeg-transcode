5d6798bfe3db8ca78acce908eddb3cad757d6d07
Gordon Schulz <gordon.schulz@gmail.com>
2020-07-01 10:34:16 +0200

    Use find and some syntactic sugar

--- rootfs/usr/local/bin/ffmpeg-transcode-dir.sh
+++ rootfs/usr/local/bin/ffmpeg-transcode-dir.sh
@@ -5,20 +5,20 @@ shopt -s nullglob
 
 [[ -z "${1}" ]] && source='.' || source="${1}"
 
-for file in $(find "${source}"\
-    \(-iname \*.flv \
+for file in $(find "${source}" \
+    \( -iname \*.flv \
     -o -iname \*.f4v \
     -o -iname \*.wmv \
     -o -iname \*.mov \
-    -o -inmae \*.mkv \
+    -o -iname \*.mkv \
     -o -iname \*.mp4 \
     -o -iname \*.avi \
-    -o -iname \*.mpg\) | sort -R); do
-        filename=$(basename -- "$file")
-        extension="${filename##*.}"
-        filename="${filename%.*}"
+    -o -iname \*.mpg \) | sort -R); do
+    filename=$(basename -- "$file")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
 
-        ffmpeg-transcode.sh -i "${file}" \
-            -f "crop=$(ffmpeg-cropdetect.sh "${file}" -q)" -o encoded && \
-            [[ -f "encoded/${filename}.mp4" ]] && rm -f "${file}"
+    ffmpeg-transcode.sh -i "${file}" \
+        -f "crop=$(ffmpeg-cropdetect.sh "${file}" -q)" -o encoded &&
+        [[ -f "encoded/${filename}.mp4" ]] && rm -f "${file}"
 done
